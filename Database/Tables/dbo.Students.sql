67b146903a417ee852530ca11d022786ff4261c6
phanchanhvn <114410218+phanchanhvn@users.noreply.github.com>
2022-11-05 14:32:03 +0700

    Add database folder and update system usecase diagram.

---
+++ Database/Tables/dbo.Students.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Students] (
+  [ID] [varchar](50) NOT NULL,
+  [Name] [nvarchar](100) NOT NULL,
+  [Phone] [varchar](20) NOT NULL,
+  [Email] [varchar](200) NOT NULL,
+  [Adress] [nvarchar](400) NULL,
+  [Birthday] [date] NULL,
+  CONSTRAINT [PK_Students_ID] PRIMARY KEY CLUSTERED ([ID])
+)
+ON [PRIMARY]
+GO
