34bfbc87c1288c5a5d9205ec8154c3d99c38e9d3
phanchanhvn <114410218+phanchanhvn@users.noreply.github.com>
2022-11-05 10:01:58 +0700

    init project

---
+++ Source/GraduateThesisManagement/GraduateThesisManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GraduateThesisManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
