e2f75d23755c935a8838983d91d013eef211d1ef
nguyenhuudongkhanh <dongkhanh2001@gmail.com>
2022-11-26 14:50:46 +0700

    add layout

---
+++ Source/GraduateThesisManagement/GraduateThesis.Web/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduateThesis.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignInStudent()
+        {
+            return View();
+        }
+        public IActionResult ResetPasswordStudent()
+        {
+            return View();
+        }
+        public IActionResult SignInLecturerst()
+        {
+            return View();
+        }
+        public IActionResult ResetPasswordLecturers()
+        {
+            return View();
+        }
+    }
+}
