fa287e15c84399343813ea7a3eb4e528bd548eab
TranHang-2605 <tranthingochang733@gmail.com>
2022-12-25 22:00:21 +0700

    Update MIME types for ContentTypeConsts

--- Source/GraduateThesisManagement/GraduateThesis.Common/File/ContentTypeConsts.cs
+++ Source/GraduateThesisManagement/GraduateThesis.Common/File/ContentTypeConsts.cs
@@ -5,11 +5,11 @@ namespace GraduateThesis.Common.File
     {
         #region Document types
 
-        public const string PDF = "";
-        public const string DOC = "text";
-        public const string DOCX = "html";
-        public const string PPT = "image";
-        public const string PPTX = "";
+        public const string PDF = "application/pdf";
+        public const string DOC = "application/msword";
+        public const string DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+        public const string PPT = "application/vnd.ms-powerpoint";
+        public const string PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
         public const string XLS = "application/vnd.ms-excel";
         public const string XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
@@ -17,31 +17,31 @@ namespace GraduateThesis.Common.File
 
         #region 
 
-        public const string RAR = "";
-        public const string ZIP = "";
+        public const string RAR = "application/vnd.rar";
+        public const string ZIP = "application/zip";
 
         #endregion
 
         #region Image types
 
-        public const string PNG = "";
-        public const string IMG = "";
-        public const string BMP = "";
-        public const string GIF = "";
+        public const string PNG = "image/png";
+        public const string JPEG = "image/jpeg";
+        public const string BMP = "image/bmp";
+        public const string GIF = "image/gif";
 
         #endregion
 
         #region Video types
 
-        public const string MP4 = "";
-        public const string AVI = "";
+        public const string MP4 = "video/mp4";
+        public const string AVI = "video/x-msvideo";
 
         #endregion
 
         #region Audio types
 
-        public const string MP3 = "";
-        public const string M4A = "";
+        public const string MP3 = "audio/mpeg";
+        public const string M4A = "audio/m4a";
 
         #endregion
     }
