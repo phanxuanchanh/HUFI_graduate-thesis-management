40e7f80c906b2ee2856030fdb98179d2bee5c162
phanchanhvn <114410218+phanchanhvn@users.noreply.github.com>
2023-01-18 13:17:18 +0700

    dashboard view (faculty staff area)

---
+++ Source/GraduateThesisManagement/GraduateThesis.Web/Areas/Lecture/Controllers/FacultyStaffDashboardController.cs
@@ -0,0 +1,22 @@
+﻿using GraduateThesis.ApplicationCore.AppController;
+using GraduateThesis.ApplicationCore.WebAttributes;
+using GraduateThesis.Common.WebAttributes;
+using GraduateThesis.Repository.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduateThesis.Web.Areas.Lecture.Controllers;
+
+[Area("Lecture")]
+[Route("lecture/dashboard")]
+[WebAuthorize]
+[AccountInfo(typeof(FacultyStaffOutput))]
+public class FacultyStaffDashboardController : WebControllerBase
+{
+    [Route("overview")]
+    [HttpGet]
+    [PageName(Name = "Trang tổng quan")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
