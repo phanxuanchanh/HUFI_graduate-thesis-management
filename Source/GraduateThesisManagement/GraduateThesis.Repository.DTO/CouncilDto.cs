ff3c2a339bb46f09663b8e76012ed3393e0d862e
phanchanhvn <114410218+phanchanhvn@users.noreply.github.com>
2023-01-28 17:28:16 +0700

    update db, fix council manager

--- Source/GraduateThesisManagement/GraduateThesis.Repository.DTO/CouncilDto.cs
+++ Source/GraduateThesisManagement/GraduateThesis.Repository.DTO/CouncilDto.cs
@@ -1,38 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
-namespace GraduateThesis.Repository.DTO
+namespace GraduateThesis.Repository.DTO;
+
+public class CouncilInput
 {
-    public class CouncilInput
-    {
-        [Display(Name = "Mã hội đồng")]
-        public string Id { get; set; }
+    [Display(Name = "Mã hội đồng")]
+    public string Id { get; set; }
 
-        [Display(Name = "Tên hội đồng")]
-        [Required(ErrorMessage = "{0} không được để trống")]
-        public string Name { get; set; }
+    [Display(Name = "Tên hội đồng")]
+    [Required(ErrorMessage = "{0} không được để trống")]
+    public string Name { get; set; }
 
-        [Display(Name = "Mô tả")]
-        public string Description { get; set; }
+    [Display(Name = "Mô tả")]
+    public string Description { get; set; }
 
-        [Display(Name = "Học kỳ")]
-        [Required(ErrorMessage = "{0} không được để trống")]
-        public int Semester { get; set; }
+    [Display(Name = "Học kỳ")]
+    [Required(ErrorMessage = "{0} không được để trống")]
+    public int? Semester { get; set; }
 
-        [Display(Name = "Năm học")]
-        [Required(ErrorMessage = "{0} không được để trống")]
-        public string Year { get; set; }
-    }
+    [Display(Name = "Năm học")]
+    [Required(ErrorMessage = "{0} không được để trống")]
+    public string Year { get; set; }
+}
 
-    public class CouncilOutput : CouncilInput
-    {
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public DateTime? DeletedAt { get; set; }
-    }
+public class CouncilOutput : CouncilInput
+{
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
 }
